47409487815565a7a489cd6569d13091d6251c4c
Justin McBride <justin.mcbride.599z@gmail.com>
2013-12-21 15:07:47 -0700

    crimecameraactivity

---
+++ CriminalIntent/src/main/java/com/justin/criminalintent/CrimeCameraActivity.java
@@ -0,0 +1,13 @@
+package com.justin.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Justin on 12/21/13.
+ */
+public class CrimeCameraActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeCameraFragment();
+    }
+}
