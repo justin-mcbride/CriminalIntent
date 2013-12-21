26a87ee8d1c6ca05e06bbfc28711e7aae3f47615
Justin McBride <justin.mcbride.599z@gmail.com>
2013-12-21 14:46:40 -0700

    new

--- CriminalIntent/src/main/java/com/justin/criminalintent/CrimeCameraFragment.java
+++ CriminalIntent/src/main/java/com/justin/criminalintent/CrimeCameraFragment.java
@@ -4,9 +4,11 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Justin on 12/21/13.
@@ -19,6 +21,18 @@ public class CrimeCameraFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime_camera, parent, false);
 
+        Button takePictureButton = (Button)v.findViewById(R.id.crime_camera_takePictureButton);
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().finish();
+            }
+        });
+
+        mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceview);
+
+        return v;
     }
 }
