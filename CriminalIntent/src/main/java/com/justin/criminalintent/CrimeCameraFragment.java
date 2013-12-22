c4885dfb39100f8d1d8d9f51ad9f3bec96f4e089
Justin <justin.mcbride.599z@gmail.com>
2013-12-22 12:43:29 -0700

    page 310

--- CriminalIntent/src/main/java/com/justin/criminalintent/CrimeCameraFragment.java
+++ CriminalIntent/src/main/java/com/justin/criminalintent/CrimeCameraFragment.java
@@ -5,6 +5,7 @@ import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Surface;
 import android.view.SurfaceHolder;
@@ -12,6 +13,9 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by Justin on 12/21/13.
@@ -38,7 +42,61 @@ public class CrimeCameraFragment extends Fragment {
         mSurfaceView = (SurfaceView)v.findViewById(R.id.crime_camera_surfaceview);
         SurfaceHolder holder = mSurfaceView.getHolder();
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        holder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                // tell the camera to use this surface as its preview area
+                try {
+                    if (mCamera != null) {
+                        mCamera.setPreviewDisplay(surfaceHolder);
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Error setting up preview display", e);
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int w, int h) {
+                if (mCamera == null) return;
+
+                // the surface has changed size, update the camera preview size
+                Camera.Parameters parameters = mCamera.getParameters();
+                Camera.Size s = getBestSupportedSize(parameters.getSupportedPictureSizes(), w, h);
+                parameters.setPreviewSize(s.width, s.height);
+                mCamera.setParameters(parameters);
+                try {
+                    mCamera.startPreview();
+                } catch (Exception e) {
+                    Log.e(TAG, "Could not start preview", e);
+                    mCamera.release();
+                    mCamera = null;
+                }
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                if (mCamera != null) {
+                    mCamera.stopPreview();
+                }
+            }
+
+        });
+
         return v;
+    }
+
+    private Camera.Size getBestSupportedSize(List<Camera.Size> sizes, int width, int height) {
+        Camera.Size bestSize = sizes.get(0);
+        int largestArea = bestSize.width * bestSize.height;
+        for (Camera.Size size: sizes) {
+            int area = size.width * size.height;
+            if (area > largestArea) {
+                bestSize = size;
+                largestArea = area;
+            }
+        }
+        return bestSize;
     }
 
     @TargetApi(9)
