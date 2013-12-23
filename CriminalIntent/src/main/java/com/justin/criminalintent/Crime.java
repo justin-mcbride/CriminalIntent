7dd094b135bafc78d8470c2f7e294508e7892aad
Justin McBride <justin.mcbride.599z@gmail.com>
2013-12-22 23:31:57 -0700

    page 340

--- CriminalIntent/src/main/java/com/justin/criminalintent/Crime.java
+++ CriminalIntent/src/main/java/com/justin/criminalintent/Crime.java
@@ -15,11 +15,13 @@ public class Crime {
     private static final String JSON_TITLE = "title";
     private static final String JSON_SOLVED = "solved";
     private static final String JSON_DATE = "date";
+    private static final String JSON_PHOTO = "photo";
 
     private UUID mId;
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private Photo mPhoto;
 
     public Date getDate() {
         return mDate;
@@ -39,6 +41,9 @@ public class Crime {
         json.put(JSON_TITLE, mTitle);
         json.put(JSON_SOLVED, mSolved);
         json.put(JSON_DATE, mDate.getTime());
+        if (mPhoto != null) {
+            json.put(JSON_PHOTO, mPhoto.toJSON());
+        }
         return json;
     }
 
@@ -49,6 +54,7 @@ public class Crime {
         }
         mSolved = json.getBoolean(JSON_SOLVED);
         mDate = new Date(json.getLong(JSON_DATE));
+        if (json.has(JSON_PHOTO)) mPhoto = new Photo(json.getJSONObject(JSON_PHOTO));
     }
 
     public boolean isSolved() {
@@ -63,7 +69,13 @@ public class Crime {
         return mId;
     }
 
+    public Photo getPhoto() {
+        return mPhoto;
+    }
 
+    public void setPhoto(Photo p) {
+        mPhoto = p;
+    }
 
     public Crime() {
         mId = UUID.randomUUID();
--- CriminalIntent/src/main/java/com/justin/criminalintent/CrimeFragment.java
+++ CriminalIntent/src/main/java/com/justin/criminalintent/CrimeFragment.java
@@ -4,6 +4,8 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.hardware.Camera;
 import android.os.Build;
 import android.os.Bundle;
@@ -25,6 +27,7 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.Calendar;
@@ -50,6 +53,7 @@ public class CrimeFragment extends Fragment {
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
     private ImageButton mPhotoButton;
+    private ImageView mPhotoView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -144,6 +148,8 @@ public class CrimeFragment extends Fragment {
             }
         });
 
+        mPhotoView = (ImageView)v.findViewById(R.id.crime_imageView);
+
         //if camera is not available, disable camera functionality
         PackageManager pm = getActivity().getPackageManager();
         boolean hasACamera = pm.hasSystemFeature(PackageManager.FEATURE_CAMERA) ||
@@ -200,7 +206,9 @@ public class CrimeFragment extends Fragment {
             // create a new photo object and attach it to the crime
             String filename = data.getStringExtra(CrimeCameraFragment.EXTRA_PHOTO_FILENAME);
             if (filename != null) {
-                Log.i(TAG, "filename: " + filename);
+                Photo p = new Photo(filename);
+                mCrime.setPhoto(p);
+                showPhoto();
             }
         }
     }
@@ -252,5 +260,28 @@ public class CrimeFragment extends Fragment {
     }
 
     public CrimeFragment() {
+    }
+
+    private void showPhoto() {
+        Photo p = mCrime.getPhoto();
+
+        BitmapDrawable b = null;
+        if (p != null) {
+            String path = getActivity().getFileStreamPath(p.getFilename()).getAbsolutePath();
+            b = PictureUtils.getScaledDrawable(getActivity(), path);
+        }
+        mPhotoView.setImageDrawable(b);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        showPhoto();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        PictureUtils.cleanImageView(mPhotoView);
     }
 }
